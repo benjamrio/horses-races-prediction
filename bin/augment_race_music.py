fa787c1de10dff6975eaa0aa7978701366af7c88
Benjamin Rio <benjamin.rio@student-cs.fr>
2022-02-05 23:38:32 +0100

    Added comments

--- bin/augment_race_music.py
+++ bin/augment_race_music.py
@@ -8,15 +8,15 @@ import utils.flatten as flt
 import argparse
 
 
-def glob_to_df(files_paths_glob, selected_columns):
-    selected_columns = [
-        "raceId",
-        "date",
-        "horse.genyId",
-        "musique",
-        "results.position",
-        "priceFirst",
-    ]
+def glob_to_df(files_paths_glob):
+    """Aggregate JSON files containing horse race data described by the glob pattern into a DataFrame, with desired columns
+
+    Arguments:
+        files_paths_glob {string} -- Glob file pattern of historic JSON files. Should be complete (end with .json)
+
+    Returns:
+        DataFrame -- Historic performances of horses
+    """
     files_paths = glob.glob(files_paths_glob)
     print(f"Data extraction from {len(files_paths)} JSON files...")
     pbar = tqdm(total=len(files_paths))
@@ -41,10 +41,30 @@ def glob_to_df(files_paths_glob, selected_columns):
 
                 performances += [horse_performance]
     pbar.refresh()
+    selected_columns = [
+        "raceId",
+        "date",
+        "horse.genyId",
+        "musique",
+        "results.position",
+        "priceFirst",
+    ]
     return pd.DataFrame(performances)[selected_columns]
 
 
 def preprocess_df(df):
+    """Preprocess an historic performances DataFrame.
+        Modifying :
+            - date : to datetime
+            - result : ceil the position to 10, attribute 10 to a non ranked performance
+            - music : keep the result positions only, to a list
+
+    Arguments:
+        df {DataFrame} -- Historic horse performances
+
+    Returns:
+        df -- Preprocessed DataFrame
+    """
     print(
         f"Preprocessing the horse performances dataframe containing {len(df)} rows..."
     )
@@ -54,25 +74,50 @@ def preprocess_df(df):
     return df
 
 
-def clean_results_position(results_position):
-    if pd.isnull(results_position):
+def clean_results_position(result_position):
+    """Ceil the result position to 10 and attribute 10 to a non ranked performance
+
+    Arguments:
+        result_position {string} -- The arriving rank of the horse (can be 'D' for disqualified)
+
+    Returns:
+        int -- corrected result position
+    """
+    if pd.isnull(result_position):
         return 10
     else:
-        return max(int(results_position), 10)
+        return max(int(result_position), 10)
 
 
-def clean_music_to_list(musique):
-    """Nettoie la musique (retire les lettres et supprime les nombres entre parenthèses)"""
-    if pd.isnull(musique):
+def clean_music_to_list(music):
+    """Supress letters (for race type) and new year indication (number between parentheses)
+
+    Arguments:
+        music {string} -- Music as given by websites
+        (a word alterning numbers and letters)
+
+    Returns:
+        int list -- cleaned music
+    """
+    if pd.isnull(music):
         return []
 
-    musique = re.sub(r"\([^)]*\)", "", musique)
-    musique = re.sub("[^1-9]", " ", musique)
+    musique = re.sub(r"\([^)]*\)", "", music)
+    musique = re.sub("[^1-9]", " ", music)
     musique = musique.split()
     return musique
 
 
 def get_music(horse):
+    """Creates augmented music from one horse's performances
+
+    Arguments:
+        horse {DataFrame} -- Horses performances, in chronological order
+
+    Returns:
+        2D np.array -- augmented music with date, cash prize, and position.
+        Each element of the list is a performance, which is a list of the 3 features
+    """
     first_music = horse["cleaned_music"].iloc[0]
     augmented_music = horse[["results.position", "priceFirst", "date"]].values
     if first_music:
@@ -86,16 +131,29 @@ def get_music(horse):
 
 
 def get_augmented_music_df(df):
+    """Retrieve the augmented music for each horse appearing in the horses' performances dataframe
+
+    Arguments:
+        df {DataFrame} -- Horses performances
+
+    Returns:
+        DataFrame -- Output of the program : DataFrame with
+    """
     print("Retrieving augmented music for each horse...")
     return df.groupby(["horse.genyId"]).apply(get_music)
 
 
 def parse_args():
+    """Create the argument parser to retrieve input and output
+
+    Returns:
+        Namespace -- arguments
+    """
     my_parser = argparse.ArgumentParser(description="Builds augmented music of horses")
     my_parser.add_argument(
         "--input_glob",
         "-i",
-        default="data/raw/2016-2018_races/historic/*",
+        default="data/raw/2016-2018_races/historic/*.json",
         help="Input JSON files glob pattern",
     )
     my_parser.add_argument(
@@ -104,15 +162,15 @@ def parse_args():
         default="data/interim/augmented_musics.csv",
         help="Path of the output directory, in which the result file augmented_musics.csv will be created",
     )
-    args = parse_args()
+    args = my_parser.parse_args()
+    return args
 
 
 def main():
-
     args = parse_args()
-    get_augmented_music_df(
-        preprocess_df(glob_to_df(args.input_glob, selected_columns))
-    ).to_csv(args.output_folder)
+    get_augmented_music_df(preprocess_df(glob_to_df(args.input_glob))).to_csv(
+        args.output_folder
+    )
 
 
 if __name__ == "__main__":
